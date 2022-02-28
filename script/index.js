de19c3e3812102ebdc54f9b388e4739e699d902f
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-02-28 15:46:41 +0100

    Started on the code and everything.

---
+++ script/index.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector("hamburger");
+const cross = document.querySelector("cross");
+const rightNav = document.querySelector("right-nav");
+const navItems = document.querySelector("nav-item");
+
+console.log(hamburger, cross);
