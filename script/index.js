5a7abc4756811d3399202087e3c2bb2c0d4044be
sofiaronnkvist <89849180+sofiaronnkvist@users.noreply.github.com>
2022-02-28 21:34:47 +0100

    Work on navbar.

--- script/index.js
+++ script/index.js
@@ -1,6 +1,16 @@
-const hamburger = document.querySelector("hamburger");
-const cross = document.querySelector("cross");
-const rightNav = document.querySelector("right-nav");
-const navItems = document.querySelector("nav-item");
+const hamburger = document.querySelector(".nav-hamburger");
+const cross = document.querySelector(".nav-cross");
+const rightNav = document.querySelector(".right-nav");
+const navItems = document.querySelectorAll(".nav-item");
 
-console.log(hamburger, cross);
+hamburger.addEventListener("click", mobileMenu);
+cross.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("closed");
+  rightNav.classList.toggle("closed");
+  cross.classList.toggle("closed");
+  navItems.forEach(item => {
+      item.classList.toggle("closed");
+  });
+}
